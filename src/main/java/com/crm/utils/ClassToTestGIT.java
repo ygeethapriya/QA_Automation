7937284bac34675d50e112aacc8050d67ed75d32
ygeethapriya <yellurigeethapriya99@gmail.com>
2022-04-22 13:23:06 +0530

    This is my new method

--- src/main/java/com/crm/utils/ClassToTestGIT.java
+++ src/main/java/com/crm/utils/ClassToTestGIT.java
@@ -5,4 +5,7 @@ public class ClassToTestGIT {
 	public void gitprac(){
 		System.out.println("I'm new to GIT HUB");
 	}
+	public void practice(){
+		System.out.println("This is my new update");
+	}
 }
